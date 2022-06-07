1ccf803ab375128e3a18cd89b792740e56e8c819
StarlightC <mail.starlightc@gmail.com>
2022-06-07 11:12:41 +0800

    change material version

--- exoplayer/build.gradle.kts
+++ exoplayer/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.5.0-alpha02")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
 
     testImplementation("junit:junit:4.13.2")
@@ -52,7 +52,7 @@ dependencies {
 
     api("com.google.android.exoplayer:exoplayer-core:2.16.0")
     api("com.google.android.exoplayer:exoplayer-hls:2.16.0")
-    implementation("com.github.StarlightC:HBVideoView_Core:v0.1.4")
+    implementation("com.github.StarlightC:HBVideoView_Core:v0.1.8")
 }
 
 afterEvaluate {
@@ -62,7 +62,6 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.starlightc"
                 artifactId = "hbvideoview_exoplayer"
-                version = "0.1.6"
             }
         }
     }
